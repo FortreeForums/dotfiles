79ee23d2209bc90431c61a0516b915a9cf65b1c6
vortex !NOiDx.cCl2 <rennofthemill@hotmail.co.uk>
2018-07-09 19:09:13 +0100

    Update prompt.zsh

--- arch/.config/zsh/prompt.zsh
+++ arch/.config/zsh/prompt.zsh
@@ -1,3 +1,12 @@
-## Prompts
+## window title {{
+function window_title() {
+  print -Pn '\e]2;ricu~\a'
+}
+## }}
+
+## prompt {{
+function prompt_vortex() {
 PROMPT=' %F{green}<%F{white}%d%F{green}> 
  %F{cyan}>> '
+}
+## }}
